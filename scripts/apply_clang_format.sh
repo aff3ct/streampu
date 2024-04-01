cd4a9f235a6441073805a81022053dc61c58580d
Adrien Cassagne <adrien.cassagne@lip6.fr>
2024-04-01 14:00:16 +0200

    Apply clang-format to the signal tracer exe.

--- scripts/apply_clang_format.sh
+++ scripts/apply_clang_format.sh
@@ -3,3 +3,4 @@
 find include -iname *.h -o -iname *.hpp -o -iname *.hxx | xargs clang-format -i
 find src -iname *.cpp -o -iname *.h -o -iname *.hpp -o -iname *.hxx | xargs clang-format -i
 find tests -iname *.cpp -o -iname *.h -o -iname *.hpp -o -iname *.hxx | xargs clang-format -i
+find signal_tracer -iname *.cpp -o -iname *.h -o -iname *.hpp -o -iname *.hxx | xargs clang-format -i
--- signal_tracer/main.cpp
+++ signal_tracer/main.cpp
@@ -9,43 +9,43 @@
 
 #include <cpptrace/cpptrace.hpp>
 
-int main()
+int
+main()
 {
-	std::cout << "aff3ct-core signal tracer is running..." << std::endl;
-	cpptrace::object_trace trace;
-	do
-	{
-		cpptrace::safe_object_frame frame;
-		// fread used over read because a read() from a pipe might not read the full frame
-		size_t res = fread(&frame, sizeof(frame), 1, stdin);
-		if (res == 0)
-		{
-			break;
-		}
-		else if ((int)res == -1)
-		{
-			perror("Something went wrong while reading from the pipe (res = '-1')");
-			break;
-		}
-		else if (res != 1)
-		{
-			std::cerr << "Something went wrong while reading from the pipe (res = '" << res << "' and "
-			          << "sizeof(frame) = '" << sizeof(frame) << "')" << std::endl;
-			break;
-		}
-		else
-		{
-			trace.frames.push_back(frame.resolve());
-		}
-	}
-	while (true);
+    std::cout << "aff3ct-core signal tracer is running..." << std::endl;
+    cpptrace::object_trace trace;
+    do
+    {
+        cpptrace::safe_object_frame frame;
+        // fread used over read because a read() from a pipe might not read the full frame
+        size_t res = fread(&frame, sizeof(frame), 1, stdin);
+        if (res == 0)
+        {
+            break;
+        }
+        else if ((int)res == -1)
+        {
+            perror("Something went wrong while reading from the pipe (res = '-1')");
+            break;
+        }
+        else if (res != 1)
+        {
+            std::cerr << "Something went wrong while reading from the pipe (res = '" << res << "' and "
+                      << "sizeof(frame) = '" << sizeof(frame) << "')" << std::endl;
+            break;
+        }
+        else
+        {
+            trace.frames.push_back(frame.resolve());
+        }
+    } while (true);
 
 #ifdef AFF3CT_CORE_COLORS
-	bool enable_color = true;
+    bool enable_color = true;
 #else
-	bool enable_color = false;
+    bool enable_color = false;
 #endif
-	trace.resolve().print(std::cerr, enable_color);
+    trace.resolve().print(std::cerr, enable_color);
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
