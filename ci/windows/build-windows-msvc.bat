a60a4d7d3f2b2da5602a8ac213682b3886f169ec
Francois Cheminade <francois.cheminade@inria.fr>
2025-02-20 10:57:01 +0100

    win runner update #7

--- ci/windows/build-windows-msvc.bat
+++ ci/windows/build-windows-msvc.bat
@@ -3,12 +3,12 @@
 call ./ci/tools/threads.bat
 
 set "VSCMD_START_DIR=%CD%"
-call "%VS_PATH%\VC\Auxiliary\Build\vcvars64.bat"
+call "c:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 cmake --version
 mkdir build
 cd build
-cmake .. -G"Visual Studio 15 2017 Win64" %CMAKE_OPT% -DCMAKE_CXX_FLAGS="%CFLAGS% /MP%THREADS%" -DCMAKE_INSTALL_PREFIX="%NAME%" -DCMAKE_INCLUDE_PATH=%CMAKE_INCLUDE_PATH% -DCMAKE_PREFIX_PATH=%CMAKE_PREFIX_PATH%
+cmake .. -G"Visual Studio 17 2022" -A x64  %CMAKE_OPT% -DCMAKE_CXX_FLAGS="%CFLAGS% /MP%THREADS%" -DCMAKE_INSTALL_PREFIX="%NAME%" -DCMAKE_INCLUDE_PATH=%CMAKE_INCLUDE_PATH% -DCMAKE_PREFIX_PATH=%CMAKE_PREFIX_PATH%
 if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
 rem devenv /build Release streampu.sln
 msbuild streampu.sln /t:Build /p:Configuration=Release
